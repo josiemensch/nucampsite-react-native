cdbc55a6d85ad40498056b73737508b3320fd042
Josephine Mensching <josiemensching72@gmail.com>
2022-05-25 09:03:04 -0400

    Alert

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -31,29 +31,31 @@ class Favorites extends Component {
             return (
                 <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
                     <View style={styles.deleteView}>
-                    <TouchableOpacity
-                            style={styles.deleteTouchable}
-                            onPress={() =>
-                                Alert.alert(
-                                    'Delete Favorite?',
-                                    'Are you sure you wish to delete the favorite campsite ' +
-                                        item.name +
-                                        '?',
-                                    [
-                                        {
-                                            text: 'Cancel',
-                                            onPress: () => console.log(item.name + 'Not Deleted'),
-                                            style: 'cancel'
-                                        },
-                                        {
-                                            text: 'OK',
-                                            onPress: () => this.props.deleteFavorite(item.id)
-                                        },
-                                    ],
-                                    { cancelable: false }
-                                )
-                            }
+                        <TouchableOpacity
+                        style={styles.deleteTouchable}
+                        onPress={() => 
+                            Alert.alert(
+                                'Delete Favorite?',
+                                'Are you sure you wish to delete the favorite campsite ' +
+                                    item.name +
+                                    '?',
+                                [
+                                    {
+                                        text: 'Cancel',
+                                        onPress: () => console.log(item.name + 'Not Deleted'),
+                                        style: 'cancel'
+                                    },
+                                    {
+                                        text: 'OK',
+                                        onPress: () => this.props.deleteFavorite(item.id)
+                                    },
+                                ],
+                                { cancelable: false }
+                            )
+                        }
                         >
+                        <Text style={styles.deleteText}>Delete</Text>
+                        </TouchableOpacity>
                     </View>
 
                     <View>
